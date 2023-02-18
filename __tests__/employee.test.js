8eb8e9bd260064c83b09ee83f6eaee76f62c6072
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-18 15:49:01 -0600

    created tests for employee

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,5 +1,28 @@
-// require employee
-// describe test (positive, negative, exeption)
-// arrange
-// act
-// assert
+const employee = require('../lib/employee')
+
+describe('employee', () => {
+    // consts for employee to test
+    const e = new employee();
+
+    it("can set name of employee", () => {
+        const name = "Zen Coh";
+        expect(e.name).toBe(name);
+    });
+
+    it("can set id of employee", () => {
+        const test = 1;
+        expect(e.id).toBe(test);
+    });
+
+    it("can set email of employee", () => {
+        const test = 'zencoh@gmail.com';
+        expect(e.email).toBe(test);
+    });
+
+    describe('getRole', () => {
+        it("getRole() returns employee", () => {
+            const test = 'employee';
+            expect(e.getRole()).toBe(test);
+        });
+    });
+})
