d34bf50bfff03cd986e6b49b12f98b12a0145835
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-18 16:01:41 -0600

    created engineer test

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,5 +1,12 @@
-// require engineer.js
-// describe test (positive, negative, exeption)
-// arrange
-// act
-// assert
+const { describe } = require('node:test');
+const engineer = require('../lib/engineer');
+
+describe('engineer', () => {
+    it("can set github account", () => {
+        const test = zencoh;
+        const e = new engineer("foo", 2, "zencoh@gmail.com", test);
+        expect(e.github).toBe(test);
+    });
+
+    // test for get role, it should return engineer
+})
