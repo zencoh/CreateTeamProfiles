af068df9e5fafd413319f62950b4c4b04b3d6025
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-18 19:14:37 -0600

    created tests for intern

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,5 +1,14 @@
-// require intern.js
-// describe test (positive, negative, exeption)
-// arrange
-// act
-// assert
+const intern = require('../lib/intern');
+
+describe('intern', () => {
+    it("can set name of school", () => {
+        const test = "Kansas University";
+        const e = new intern("", 3, "", test);
+        expect(e.school).toBe(test);
+    });
+
+    it("can set role", () => {
+        const e = new intern(); 
+        expect(e.getRole()).toBe('intern');
+    });
+})
