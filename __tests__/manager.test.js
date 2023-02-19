bee30711bdbf3f58dd8b54cd17aa15045f581908
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-18 19:16:47 -0600

    created tests for manager

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -1,5 +1,14 @@
-// require manager
-// describe test (positive, negative, exeption)
-// arrange
-// act
-// assert
+const manager = require('../lib/manager');
+
+describe('manager', () => {
+    it("can set github account", () => {
+        const test = 1234;
+        const e = new manager("", 4, "", test);
+        expect(e.officeNumber).toBe(test);
+    });
+
+    it("can set role", () => {
+        const e = new manager(); 
+        expect(e.getRole()).toBe('manager');
+    });
+})
