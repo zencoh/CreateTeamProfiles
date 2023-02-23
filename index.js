5c08640b1aad87b95e62be983762786d349d022b
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-23 10:26:28 -0600

    moved the generated html outside of dist so that github can creat a page

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ function generateHtml() {
             console.log(err);
         } else {
             const mainHtmlPage = data.replace('<!-- {{employee}} -->', html);
-            fs.writeFile('./dist/teamProfiles.html', mainHtmlPage, (err) => {
+            fs.writeFile('./teamProfiles.html', mainHtmlPage, (err) => {
                 if (err)
                 console.log(err);
                 else {
